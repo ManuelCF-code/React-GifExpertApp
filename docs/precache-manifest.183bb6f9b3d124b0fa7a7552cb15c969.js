a03cf5dff15c44e4b6ab3185a495d56cabcc8b35
ManuelCF code <triviumtmbnlr@gmail.com>
2020-08-26 16:38:06 -0500

    docs agregado

---
+++ docs/precache-manifest.183bb6f9b3d124b0fa7a7552cb15c969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7fa89bd8c10537555222aa4d631a9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e076b506022f3774272",
+    "url": "/static/css/main.e61a0f93.chunk.css"
+  },
+  {
+    "revision": "4fb4b4aa597af662ffc0",
+    "url": "/static/js/2.f1c5b5b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f1c5b5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e076b506022f3774272",
+    "url": "/static/js/main.9a169a14.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
